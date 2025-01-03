85f19c1535a978a5d45a85f09dfc6434328652a3
sandervandevelde <svelde@gmail.com>
2025-01-03 13:39:15 +0100

    added meadow logic

--- README.md
+++ README.md
@@ -4,6 +4,52 @@
 
 Cycle your way to Streetview, from location to location. 
 
+## How does it work?
+
+The streetview app runs a nodejs webserver app.
+
+It also supports a browser app (written in typescript) to show a streetview panorama.
+
+The browser app fetches a cycle signal from theserver app to show the next panorama.
+
+A meadow app connects to the server app and sends a new cycle signal.
+
+So, the meadow controls the streetview panorama.
+
+It's up to you to add logic for sending that cycle signal.
+
+## Prerequisites
+
+You need:
+
+- Visual Studio with Wilderness labs Meadow extension 
+- Git
+- Node.JS
+- Visual Studio Code (optional to maintain the server app)
+
+## Starting the application
+
+Follow the following steps:
+
+0. Clone this repo
+1. Browse to the streetview-src folder
+2. Perform 'npm install'
+3. Perform 'npm start'
+4. Accept firewall questions for both public AND private networks
+5. See the server app is running
+6. The server accepts 'o' to open a browser showing streetview (a black screen is normal)
+7. The server accepts 'q' to close the app
+8. Note the ip address of your server
+9. In the sourcecode of the meadow, fill in the Wifi SSID, Wifi password, and the IP address of the server app
+10. Deploy the meadow code in Visual Studio
+11. In the meadow console, see the wifi is connected
+12. In the meadow console, see website is called
+13. In the server app, see the incoming cycle calls
+
+## License
+
+This repository is available under MIT license.
+
 ## Contributions
 
 If you want to contribute, plaese provide a pull request.
---
+++ meadow-src/MeadowApp.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Leds;
+using Meadow.Hardware;
+using Meadow.Peripherals.Leds;
+
+namespace Project1
+{
+    public class MeadowApp : App<F7FeatherV2>
+    {
+        private static int _counter = 0;
+        public override Task Initialize()
+        {
+            Resolver.Log.Info("Initialize...");
+
+            var wifi = Device.NetworkAdapters.Primary<IWiFiNetworkAdapter>();
+
+            wifi.NetworkDisconnected += (s, e) => Resolver.Log.Info("Disconnected event...");
+            wifi.NetworkConnectFailed += (s) => Resolver.Log.Info("Connect failed event...");
+            wifi.NetworkConnecting += (s) => Resolver.Log.Info("Connecting event...");
+            wifi.NetworkError += (s, e) => Resolver.Log.Info($"Error event: {e.ErrorCode}");
+            wifi.NetworkConnected += NetworkConnected;
+
+            try
+            {
+                Resolver.Log.Info($"Start connecting.....");
+
+                wifi.Connect("gspot", "sinterklaas", TimeSpan.FromSeconds(60));
+
+                Resolver.Log.Info($"Is connected.....");
+            }
+            catch (Exception ex)
+            {
+                Resolver.Log.Info($"Connection exception: {ex.Message}");
+            }
+
+            //            Resolver.Log.Info($"SSID: {wifi.DefaultSsid}");
+
+            return base.Initialize();
+        }
+
+        private async void NetworkConnected(INetworkAdapter sender, NetworkConnectionEventArgs args)
+        {
+            while (true)
+            {
+                await Cycle();
+
+                await Task.Delay(TimeSpan.FromMilliseconds(5000));
+            }
+        }
+
+        static async Task Cycle()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    _counter++;
+
+                    HttpResponseMessage response =
+                        await client.GetAsync($"http://192.168.0.208:7000/cycle?timestamp={_counter}");
+
+                    response.EnsureSuccessStatusCode();
+                    string json = await response.Content.ReadAsStringAsync();
+
+                    Resolver.Log.Info($"Cycle: {_counter}, Timestamp: {json}");
+                }
+                catch (TaskCanceledException)
+                {
+                    Resolver.Log.Info("Request timed out.");
+                }
+                catch (Exception e)
+                {
+                    Resolver.Log.Info($"Request went sideways: {e.Message}");
+                }
+            }
+        }
+    }
+
+    public class GetCycle
+    {
+        public int timestamp { get; set; }
+    }
+}
